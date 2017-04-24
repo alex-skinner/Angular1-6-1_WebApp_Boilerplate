9e0f6bd76cf7253cc81512c5c30d46f57421b494
Alex Skinner <alex.skinner@redpixie.com>
2017-04-24 11:29:52 +0100

    - Boiler plate set up

--- express.js
+++ express.js
@@ -6,4 +6,13 @@ var app = express();
 
 app.use(express.static(__dirname));
 
-app.listen(process.env.PORT || 3000);
+app.use('/bower', express.static(__dirname + "/bower_components"));
+app.use(express.static(__dirname + "/app", {
+    extensions: ['htm', 'html'],
+    redirect: true
+}));
+
+var port = process.env.PORT || 30000;
+
+app.listen(port);
+console.log("App hosting on port: " +port);
